5d9aa0a3e02d3591d5c40033fe1ebf37166f1c1a
david espinal <despinal0425@gmail.com>
2021-04-27 19:12:58 -0400

    added express

--- seed/brandsProducts.js
+++ seed/brandsProducts.js
@@ -15,12 +15,33 @@ const main = async () => {
   const brand2 = await new Brand({ name: "Vespa", url: "https://www.vespa.com" });
   brand2.save();
 
+  const brand3 = await new Brand({ name: 'New Balance', url: 'https://www.newbalance.com' })
+  brand3.save()
 
-
-
-
+  const brand4 = await new Brand({ name: 'Tribe', url: 'https://www.tribebicycles.com' })
+  brand4.save()
+  
+  const brand5 = await new Brand({ name: 'Stumptown', url: 'https://www.stumptowncoffee.com' })
+  brand5.save()
 
   const products = [
-    {title: ""}
+    { title: 'Apple AirPods', description: 'https://www.apple.com/airpods', price: '250', brand: brand1._id },
+    { title: 'Apple iPhone Pro', description: 'https://www.apple.com/iphone-11-pro', price: '1000', brand: brand1._id },
+        { title: 'Apple Watch', description: 'https://www.apple.com/watch', price: '499', brand: brand1._id },
+        { title: 'Vespa Primavera', description: 'https://www.vespa.com/us_EN/vespa-models/primavera.html', price: '3000', brand: brand2._id },
+        { title: 'New Balance 574 Core', description: 'https://www.newbalance.com/pd/574-core/ML574-EG.html', price: '84', brand: brand3._id },
+        { title: 'Tribe Messenger Bike 004', description: 'https://tribebicycles.com/collections/messenger-series/products/mess-004-tx', price: '675', brand: brand4._id },
+        { title: 'Stumptown Hair Bender Coffee', description: 'https://www.stumptowncoffee.com/products/hair-bender', price: '17', brand: brand5._id }
   ]
+
+  await Product.insertMany(products);
+  console.log("Created many products");
+
 }
+
+const run = async () => {
+  await main();
+  db.close();
+}
+
+run();
